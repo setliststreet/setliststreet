f674d624dbacfc7339465813d207c9949dbf1bb5
App Engineer <appengineer7@gmail.com>
2025-07-24 14:08:04 +0530

    feat: ui-changes

--- deadco-setlist-game/components/MainLayout.tsx
+++ deadco-setlist-game/components/MainLayout.tsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div
-        className="min-h-screen bg-[url('/images/bg.jpg')] bg-no-repeat bg-center bg-cover"
+        className="min-h-screen bg-[url('/elements/background3.png')] bg-no-repeat bg-center bg-cover"
       >
      <Header />
       <main>{children}</main>
--- deadco-setlist-game/next.config.ts
+++ deadco-setlist-game/next.config.ts
@@ -1,23 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export", // ✅ This makes Next.js generate static HTML in `out/`
+output: "export",
 
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
+images: {
+unoptimized: true,
+},
+
+eslint: {
+ignoreDuringBuilds: true,
+},
+typescript: {
+ignoreBuildErrors: true,
+},
 };
 
 export default nextConfig;
-
-
-
-
